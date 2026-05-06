c0def095a98cbedb521862b5af0c66b0b2e90f32
Bob Cozzi <cozzi@rpgiv.com>
2026-05-06 09:23:05 -0500

    Rename SPLUTILS.H→COZUTILS.H, spu_→coz_ prefix; add BLDUDTF docs and cmd; reorganize source dirs; ignore QSYSINC; set SCRATCHPAD 6000

---
+++ src/build/bldudtf.cmd
@@ -0,0 +1,37 @@
+ BLDUDTF:    CMD        PROMPT('Create SQL Tools OSS UDTF')
+             PARM       KWD(UDTF) TYPE(*NAME) LEN(10) MIN(1) +
+                          EXPR(*YES) PROMPT('UDTF Source member')
+             PARM       KWD(EXTPGM) TYPE(*CHAR) LEN(10) MIN(1) +
+                          EXPR(*YES) PROMPT('External program name')
+
+             PARM       KWD(EXTPGMSRC) TYPE(EXTPS) +
+                          PROMPT('External program source file')
+ EXTPS:      QUAL       TYPE(*NAME) LEN(10) DFT(QCSRC) +
+                          SPCVAL((QCSRC)) EXPR(*YES)
+             QUAL       TYPE(*NAME) LEN(10) DFT(SQLTOOLS) EXPR(*YES) +
+                          PROMPT('Source library')
+             PARM       KWD(EXTPGMMBR) TYPE(*NAME) LEN(10) +
+                          DFT(*EXTPGM) SPCVAL((*EXTPGM)) +
+                          PROMPT('External program source member')
+
+             PARM       KWD(UDTFSRC) TYPE(UDFSRC) +
+                          PROMPT('UDTF Source file')
+ UDFSRC:     QUAL       TYPE(*NAME) LEN(10) DFT(QUDFSRC) +
+                          SPCVAL((QUDFSRC) (QSQLSRC)) EXPR(*YES)
+             QUAL       TYPE(*NAME) LEN(10) DFT(SQLTOOLS) EXPR(*YES) +
+                          PROMPT('Source library')
+             PARM       KWD(OBJLIB) TYPE(*CHAR) LEN(10) +
+                          DFT(SQLTOOLS) PROMPT('Target Library')
+             PARM       KWD(DBGVIEW) TYPE(*CHAR) LEN(8) RSTD(*YES) +
+                          DFT(*SOURCE) SPCVAL((*NONE) +
+                          (*STMT) (*SOURCE) (*LIST)) PROMPT('Debug +
+                          view')
+             PARM       KWD(DROP) TYPE(*LGL) RSTD(*YES) DFT(*NO) +
+                          SPCVAL((*YES '1') (*NO '0') (*DROP '1')) +
+                          EXPR(*YES) PROMPT('DROP FUNCTOIN before +
+                          create')
+             PARM       KWD(SPECNAME) TYPE(*PNAME) LEN(128) +
+                          DFT(*UDTF) SPCVAL((*UDTF)) EXPR(*YES) +
+                          PMTCTL(DROPNAME) PROMPT('Specific name +
+                          for DROP')
+ DROPNAME:   PMTCTL     CTL(DROP) COND((*EQ '1')) NBRTRUE(*EQ 1)
